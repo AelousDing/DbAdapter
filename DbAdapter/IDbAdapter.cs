7a9eb645675fd301010b4b9c66e11b841d14d795
fengjuanchensha <1130217572@qq.com>
2021-05-11 17:33:34 +0800

    11
    11

---
+++ DbAdapter/IDbAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAdapter
+{
+    public interface IDbAdapter
+    {
+        List<T> ToList<T>(QueryModel<T> query);
+        Task<List<T>> ToListAsync<T>();
+    }
+}
---
+++ DbAdapter/QueryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAdapter
+{
+    /// <summary>
+    /// select * from T
+    /// where **
+    /// order by **
+    /// group by **
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class QueryModel<T>
+    {
+        public Expression<Func<T,bool>> Where { get; set; }
+        public Expression<Action<T>> OrderBy { get; set; }
+
+    }
+}
